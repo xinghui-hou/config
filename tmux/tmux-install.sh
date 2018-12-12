5da5b2e01be94025ed348a0c96a67ef08816e9d7
侯星辉 <houxinghui@houxinghuis-MacBook-Pro.local>
2018-12-12 16:30:32 +0800

    fix(): fix tmux-install

--- tmux/tmux-install.sh
+++ tmux/tmux-install.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
 DIR="/usr/local/libevent";
-yum -y install ncurses-devel gcc gcc-c++ make
+yum -y install ncurses-devel gcc gcc-c++ make wget
 
-wget https://github.com/downloads/libevent/libevent/libevent-2.0.20-stable.tar.gz
-tar xvzf libevent-2.0.20-stable.tar.gz
+[ -f libevent-2.0.20-stable.tar.gz ] || wget https://github.com/downloads/libevent/libevent/libevent-2.0.20-stable.tar.gz
+tar vzf libevent-2.0.20-stable.tar.gz
 cd libevent-2.0.20-stable
 ./configure --prefix=$DIR
 make && make install
 
-ln-s /usr/loal/libevent/lib/libevent-2.0.so.5 /usr/lib64/libevent-2.0.so.5
+ln -s /usr/local/libevent/lib/libevent-2.0.so.5 /usr/lib64/
 
-wget https://github.com/tmux/tmux/releases/download/2.6/tmux-2.6.tar.gz
-
-[ -f tmux-2.6.tar.gz  ] && tar zxvf tmux-2.6.tar.gz
+[ -f tmux-2.6.tar.gz  ] || wget https://github.com/tmux/tmux/releases/download/2.6/tmux-2.6.tar.gz
+[ -f tmux-2.6.tar.gz  ] && tar zvf tmux-2.6.tar.gz
 cd tmux-2.6
+echo /configure --prefix=/usr/local/tmux CFLAGS="-I$DIR/include" LDFLAGS="-L$DIR/lib"
 ./configure --prefix=/usr/local/tmux CFLAGS="-I$DIR/include" LDFLAGS="-L$DIR/lib"
 make && make install
+cd ..
 cp /usr/local/tmux/bin/tmux /usr/local/bin/
 cp tmux.conf ~/.tmux.conf
 
